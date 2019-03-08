0685421ffc2e48d3672f1be765e6832ccfebbb2b
Wayne Collier <ke4tri@gmail.com>
2019-03-07 19:14:39 -0600

    refactoring

--- CompanyClasses/Program.cs
+++ CompanyClasses/Program.cs
@@ -8,5 +8,35 @@ namespace CompanyClasses
         {
             Console.WriteLine("Hello World!");
         }
+
+        public class Company
+        {
+            /*
+                Some readonly properties
+            */
+            public string Name { get; }
+            public DateTime CreatedOn { get; }
+            public string CurrentEmps { get; }
+
+            // Create a property for holding a list of current employees
+
+            // Create a method that allows external code to add an employee
+            public void AddEmployee(string empoloyee)
+            {
+                Console.WriteLine(empoloyee);
+            }
+            // Create a method that allows external code to remove an employee
+            public void RemoveEmployee(string empoloyee)
+            {
+                Console.WriteLine(empoloyee);
+            }
+            /*
+                Create a constructor method that accepts two arguments:
+                    1. The name of the company
+                    2. The date it was created
+
+                The constructor will set the value of the public properties
+            */
+        }
     }
 }
